db88098df99d2171bb0d182feda1c2d2b1e47f83
hyj511 <hyj511@outlook.com>
2017-06-26 19:40:22 +0800

    2th change

--- MiniProgram/pages/productDetail/productDetail.js
+++ MiniProgram/pages/productDetail/productDetail.js
@@ -5,62 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    swiper: {
+      images: ['1', '2', '3', '4', '5', '6', '7']//TODO get data from server
+    },
+    users: ['1', '2'],
+    modalRetreatHidden: true,
+    modalFireHidden: true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  /*
+    Called when user click question mark in 七天包退, 十四天包换 to show modal
+  */
+  modalRetreat: function (e) {
+    this.setData({
+      modalRetreatHidden: false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  modalRetreatChange: function (e) {
+    this.setData({
+      modalRetreatHidden: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  /*
+    Called when user click question mark in 火热拼团 to show modal
+  */
+  modalFire: function (e) {
+    this.setData({
+      modalFireHidden: false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  modalFireChange: function (e) {
+    this.setData({
+      modalFireHidden: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
